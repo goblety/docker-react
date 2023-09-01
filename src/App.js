2dc0c8e12d1820cea90b2eab68a98c475793a7af
goblety <tim.suhadolnik@gmail.com>
2023-09-01 11:28:48 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          zdej mogl delati bi
+          I was changed on dah feture branch
         </p>
         <a
           className="App-link"
